a6065a02838952856611d7a4f99d9f582d69e6e9
jilson jimenez <jilsonjimenez@gmail.com>
2021-11-26 16:26:48 -0500

    add key and component welcome

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Welcome from './Welcome'
 
 class App extends Component {
   constructor() {
@@ -14,7 +15,7 @@ class App extends Component {
     return (
       <div>
         {this.state.names.map((name) =>
-          <Welcome name={name} />
+          <Welcome key={name} name={name} />
         )}
       </div>
     );
---
+++ src/Welcome.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+
+const Welcome = ({name}) => <h1 key={`llave${name}`} >Hola {name}</h1>
+
+export default Welcome;
